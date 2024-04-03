7265c0bbe2a693bb7c62fb7ebac434e17115743a
Stein Ortiz <stein.ortiz@gmail.com>
2024-04-03 17:41:10 -0300

    Mecanica de control de la bola hecha... por implementar

--- Assets/Scripts/BouncingBall/SuperStrike.cs
+++ Assets/Scripts/BouncingBall/SuperStrike.cs
@@ -8,12 +8,16 @@ public class SuperStrike : MonoBehaviour
 {
     private Rigidbody rb;
     public bool canDo;
+    [Range(0f, 1f)] public float influence;
     public float fallSpeed;
     public float bounceSpeed;
     public float minimalBounceSpeed;
     public float moveSpeed;
-    public float roce;
-    
+    [Range(0f,1f)] public float roce;
+    [Range(0f,1f)] public float roceVertical;
+    [Range(0f, 1f)] public float roceHorizontal;
+
+
     // Start is called before the first frame update
     void Start()
     {
@@ -23,59 +27,80 @@ public class SuperStrike : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if (Input.GetButtonDown("Jump") && (canDo || rb.velocity.normalized.y <0))
-        {
-            BoostSpeed(Vector3.down, fallSpeed);
-        }
-
+    
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
         Vector3 movement = new Vector3(x, 0, z);
-        transform.Translate(movement * moveSpeed * Time.deltaTime);
+        //transform.Translate(movement * moveSpeed * Time.deltaTime); NO DEBE SER PORQUE GENERA RESISTENCIA CUANDO LA VELOCIDAD VA A HACIA UN LADO Y lo mueves al otro
+        rb.velocity += movement * (moveSpeed * Time.deltaTime);
         
-        if (rb.velocity.z > 0f)
+        if (Input.GetButtonDown("Jump") && (canDo || rb.velocity.normalized.y <0))
         {
-            rb.velocity -=Vector3.forward*roce;
-        }
-        if (rb.velocity.x > 0f)
-        {
-            rb.velocity -=Vector3.right*roce;
+            BoostSpeed(fallSpeed, Vector3.down + influence * movement);
         }
         
-        if (rb.angularVelocity.magnitude > 0f)
-        {
-            rb.angularVelocity =Vector3.zero;
-        }
+        Vector3 dragMagnitude = roce *rb.velocity.sqrMagnitude * rb.velocity.normalized;
+        rb.velocity -= dragMagnitude * Time.deltaTime;
     }
 
-    public void CollisionBoost(float boost)
+    void Rose()
     {
-        BoostSpeed(rb.velocity.normalized, boost);
+        var c = 0.1f;
+        var dragMagnitude = c *rb.velocity.sqrMagnitude * rb.velocity.normalized;
     }
 
-    void BoostSpeed(Vector3 dir,float boost)
+    
+    void BoostSpeed(float boost,Vector3 dir)
     {
         rb.velocity += dir*boost;
+    }
+    void BoostSpeed(float boost)
+    {
+        BoostSpeed(boost,rb.velocity.normalized);
     }
 
     private void OnCollisionEnter(Collision collision)
     {
-        if (collision.relativeVelocity.magnitude > minimalBounceSpeed)
+        if (collision.collider.tag == "ground")
         {
-            if (collision.relativeVelocity.magnitude > bounceSpeed)
+            //Colision Velocidad en Y
+            if (collision.relativeVelocity.magnitude > minimalBounceSpeed)
             {
-                rb.velocity = rb.velocity.normalized*bounceSpeed;
+                if (collision.relativeVelocity.magnitude > bounceSpeed)
+                {
+                    // Si es mayor que salga a la bounceSpeed (efecto cartoon)
+                    rb.velocity = rb.velocity.normalized*bounceSpeed;
+                }
+                else
+                {
+                    // si es igual o menor a la bounceSpeed  que decaiga con taza roce Vertical)
+                    rb.velocity = rb.velocity.normalized*((rb.velocity.magnitude - minimalBounceSpeed*roceVertical)); 
+                }
+            
             }
             else
             {
-                rb.velocity = rb.velocity.normalized*((rb.velocity.magnitude + minimalBounceSpeed) / 2); 
+                rb.velocity = rb.velocity.normalized*((rb.velocity.magnitude + minimalBounceSpeed*roceVertical)); 
             }
             
+            // Velocidad en Z
+            if (rb.velocity.z != 0f)
+            {
+                rb.velocity -= Vector3.forward * (roceHorizontal * rb.velocity.z);
+
+            }
+            // Velocidad en X
+            if (rb.velocity.x != 0f)
+            {
+                rb.velocity -= Vector3.right * (roceHorizontal * rb.velocity.x);
+            }
+            // Velocidad Angular
+            if (rb.angularVelocity.magnitude > 0f)
+            {
+                rb.angularVelocity =Vector3.zero;
+            }
         }
-        else
-        {
-            rb.velocity = rb.velocity.normalized * minimalBounceSpeed;
-        }
+        
     }
 }
